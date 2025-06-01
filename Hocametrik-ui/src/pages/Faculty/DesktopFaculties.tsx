b628dddfd898dde64aac8cb72be7d69bdae1af7e
Harun Ider <harunider059@gmail.com>
2025-06-01 03:37:13 +0300

    add privacy policy page

--- Hocametrik-ui/src/pages/Faculty/DesktopFaculties.tsx
+++ Hocametrik-ui/src/pages/Faculty/DesktopFaculties.tsx
@@ -50,18 +50,18 @@ export default function DesktopFaculties({ faculties, universityId }: Props) {
                   <AnimatePresence mode="wait">
                     <motion.div
                       key={Icon.toString()}
-                      initial={{ 
-                        opacity: 0, 
+                      initial={{
+                        opacity: 0,
                         scale: 0.5,
                         rotate: -180
                       }}
-                      animate={{ 
-                        opacity: 1, 
+                      animate={{
+                        opacity: 1,
                         scale: 1,
                         rotate: 0
                       }}
-                      exit={{ 
-                        opacity: 0, 
+                      exit={{
+                        opacity: 0,
                         scale: 0.5,
                         rotate: 180
                       }}
